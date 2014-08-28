e49d7ec8d2bf119ea35fb2a81f4572fcaa0b69bd
Sridhar Ratnakumar <github@srid.name>
2014-08-28 13:15:00 -0700

    prep for dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='filearchive',
-    version="1.0",
+    version="1.1dev",
     description='Python module to work with file archives',
     long_description=read('README.md'),
     author='Sridhar Ratnakumar',
